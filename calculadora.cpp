b8a628b3c80e4558b93c039d8c8bea9e8dabe6b7
brenndol <brendo1994@gmail.com>
2023-10-20 20:29:46 -0300

    metodo subtraçao

--- calculadora.cpp
+++ calculadora.cpp
@@ -6,11 +6,11 @@ using namespace std;
 class Calculadora{
     public:
     int a, b;
-
+    
     int soma(int _a, int _b){
         return a + b;
     }
-
+    
     /*
         Multiplicação: Leane
         Divisão: Ericka
@@ -21,6 +21,12 @@ class Calculadora{
 
 
 int main(){
+    
+    int a=20,
+        b=10;
+
+    cout << "20 - 10 = " << a-b;
+    
 
     return 0;
 }
