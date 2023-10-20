a4fa81c0173184f683964644e7f3c0c36480d7ee
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2023-10-20 20:09:39 -0300

    Criacao da classe Calculdara

---
+++ calculadora.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+
+class Calculadora{
+    public:
+    int a, b;
+
+    int soma(int _a, int _b){
+        return a + b;
+    }
+
+    /*
+        Multiplicação: Leane
+        Divisão: Ericka
+        Subtração: Brenndol
+    */
+
+};
+
+
+int main(){
+
+    return 0;
+}
